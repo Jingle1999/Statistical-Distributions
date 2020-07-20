aa0d3e699881831d5a2b9dffc6ada4466608c07f
Andreas <65500947+Jingle1999@users.noreply.github.com>
2020-07-20 21:27:08 +0200

    Add files via upload

---
+++ test_gaussian.py
@@ -0,0 +1,10 @@
+from distributions import Gaussian
+
+gaussian_one = Gaussian(10, 5)
+
+print('Mean:', gaussian_one.mean)
+print('Std. deviation:', gaussian_one.stdev)
+
+import distributions 
+print(distributions.__file__)
+
